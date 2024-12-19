55de1b3ef6c6599ff1820a8d01996fbbe5746bc6
Jo Boettger <rodriguesjo8@outlook.com>
2024-12-19 00:59:58 -0300

    Programa funcionando sem as regras de negocio

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,39 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("number: ");
+		int number = sc.nextInt();		
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		account.withdraw(amount);
+		
+		System.out.printf("New balance: %.2f\n", account.getBalance());
+		
+		sc.close();
 
 	}
 
